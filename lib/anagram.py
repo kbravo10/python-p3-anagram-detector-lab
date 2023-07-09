4cc81daab2854012ce66e5308bfb80dfeaf5392c
Kevin Bravo <kbravo1024@gmail.com>
2023-07-09 15:20:01 -0700

    complete anagram lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+    
+    def match(self, word_list):
+        anagram_list = []
+        for word in word_list:
+            if sorted(word) == sorted(self.word):
+                anagram_list.append(word)
+        
+        return anagram_list
